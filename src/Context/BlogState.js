91af24981f662bb029ccdfbd92a30b3e4641cbf8
sgtudeveloper <webdeveloper.igcsm@gmail.com>
2024-08-17 12:08:22 +0530

    netlify check

--- src/Context/BlogState.js
+++ src/Context/BlogState.js
@@ -2,7 +2,7 @@ import BlogContext from "./BlogContext";
 
 const BlogState = (props)=>{
     const company = "IGCSM"
-    const news = "Indian nonprofit IGCSM empowers underprivileged with education, skill development, microfinance. ISO certified, collaborates for sustainable impact, reaching 50,000+ people since 2008."
+    const news = "Indian nonprofit IGCSM empowers underprivileged with education, skill development, microfinance. ISO certified, collaborates for sustainable impact, reaching 50,000+ people since 2008. netlify"
     return(
         <BlogContext.Provider 
         // value here which we want to export
