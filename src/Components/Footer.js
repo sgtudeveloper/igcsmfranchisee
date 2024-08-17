9e5f1f00614dc4127821a461c4208b9f80806be8
sgtudeveloper <webdeveloper.igcsm@gmail.com>
2024-08-17 13:47:29 +0530

    added the article on footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -59,26 +59,26 @@ export default function Footer() {
               <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
                 {/* <!-- Links --> */}
                 <h6 class="text-uppercase fw-bold mb-4 text-orange">
-                  Products
+                  Articles
                 </h6>
                 <p>
                   <a href="#!" class="text-reset">
-                    Angular
+                    Academics
                   </a>
                 </p>
                 <p>
                   <a href="#!" class="text-reset">
-                    React
+                    Research
                   </a>
                 </p>
                 <p>
                   <a href="#!" class="text-reset">
-                    Vue
+                    Programmes
                   </a>
                 </p>
                 <p>
                   <a href="#!" class="text-reset">
-                    Laravel
+                    Career
                   </a>
                 </p>
               </div>
