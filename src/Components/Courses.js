5099a4f7934185bc0670e79341863d3c104eb2e8
sgtudeveloper <webdeveloper.igcsm@gmail.com>
2024-08-20 10:59:39 +0530

    added course page

--- src/Components/Courses.js
+++ src/Components/Courses.js
@@ -4,7 +4,7 @@ export default function Courses() {
   return (
     <>
       <div className="container">
-        <h1 className="text-center">Our Courses</h1>
+        {/* <h1 className="text-center">Our Courses</h1> */}
         <div class="container h-100s">
           <div class="row align-middle">
             <div class="col-md-6 col-lg-4 column">
@@ -14,11 +14,11 @@ export default function Courses() {
                     Online <br />
                     Examination
                   </h1>
-                  <p>Visual communication and problem-solving</p>
+                  <p>Examination will be done via online mode.</p>
                 </div>
                 <a href="#">more</a>
                 <div class="ico-card">
-                  <i class="fa fa-rebel"></i>
+                  <i class="fa fa-chalkboard-user"></i>
                 </div>
               </div>
             </div>
@@ -29,11 +29,11 @@ export default function Courses() {
                     Online <br />
                     Support
                   </h1>
-                  <p>How design is implemented on the web.</p>
+                  <p>Get online support from our expert team.</p>
                 </div>
                 <a href="#">more</a>
                 <div class="ico-card">
-                  <i class="fa fa-codepen"></i>
+                  <i class="fa fa-phone"></i>
                 </div>
               </div>
             </div>
@@ -44,17 +44,83 @@ export default function Courses() {
                     Online <br />
                     Verification
                   </h1>
-                  <p>User Interface and User Experience Design.</p>
+                  <p>Get verification done through online mode.</p>
                 </div>
                 <a href="#">more</a>
                 <div class="ico-card">
-                  <i class="fa fa-empire"></i>
+                  <i class="fa fa-copy"></i>
                 </div>
               </div>
             </div>
           </div>
         </div>
       </div>
+      {/* Courses Cards */}
+
+      <hr className="w-25 text-black" />
+
+      <div className="container d-flex ">
+        {/* /* From Uiverse.io by gharsh11032000   */}
+
+        <div class="cardcourse m-2">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+            <path d="M20 5H4V19L13.2923 9.70649C13.6828 9.31595 14.3159 9.31591 14.7065 9.70641L20 15.0104V5ZM2 3.9934C2 3.44476 2.45531 3 2.9918 3H21.0082C21.556 3 22 3.44495 22 3.9934V20.0066C22 20.5552 21.5447 21 21.0082 21H2.9918C2.44405 21 2 20.5551 2 20.0066V3.9934ZM8 11C6.89543 11 6 10.1046 6 9C6 7.89543 6.89543 7 8 7C9.10457 7 10 7.89543 10 9C10 10.1046 9.10457 11 8 11Z"></path>
+          </svg>
+          <div class="card__content">
+            <p class="card__title">Card Title</p>
+            <p class="card__description">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco.
+            </p>
+          </div>
+        </div>
+        <div class="cardcourse m-2">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+            <path d="M20 5H4V19L13.2923 9.70649C13.6828 9.31595 14.3159 9.31591 14.7065 9.70641L20 15.0104V5ZM2 3.9934C2 3.44476 2.45531 3 2.9918 3H21.0082C21.556 3 22 3.44495 22 3.9934V20.0066C22 20.5552 21.5447 21 21.0082 21H2.9918C2.44405 21 2 20.5551 2 20.0066V3.9934ZM8 11C6.89543 11 6 10.1046 6 9C6 7.89543 6.89543 7 8 7C9.10457 7 10 7.89543 10 9C10 10.1046 9.10457 11 8 11Z"></path>
+          </svg>
+          <div class="card__content">
+            <p class="card__title">Card Title</p>
+            <p class="card__description">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco.
+            </p>
+          </div>
+        </div>
+        <div class="cardcourse m-2">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+            <path d="M20 5H4V19L13.2923 9.70649C13.6828 9.31595 14.3159 9.31591 14.7065 9.70641L20 15.0104V5ZM2 3.9934C2 3.44476 2.45531 3 2.9918 3H21.0082C21.556 3 22 3.44495 22 3.9934V20.0066C22 20.5552 21.5447 21 21.0082 21H2.9918C2.44405 21 2 20.5551 2 20.0066V3.9934ZM8 11C6.89543 11 6 10.1046 6 9C6 7.89543 6.89543 7 8 7C9.10457 7 10 7.89543 10 9C10 10.1046 9.10457 11 8 11Z"></path>
+          </svg>
+          <div class="card__content">
+            <p class="card__title">Card Title</p>
+            <p class="card__description">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco.
+            </p>
+          </div>
+        </div>
+        <div class="cardcourse m-2">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+            <path d="M20 5H4V19L13.2923 9.70649C13.6828 9.31595 14.3159 9.31591 14.7065 9.70641L20 15.0104V5ZM2 3.9934C2 3.44476 2.45531 3 2.9918 3H21.0082C21.556 3 22 3.44495 22 3.9934V20.0066C22 20.5552 21.5447 21 21.0082 21H2.9918C2.44405 21 2 20.5551 2 20.0066V3.9934ZM8 11C6.89543 11 6 10.1046 6 9C6 7.89543 6.89543 7 8 7C9.10457 7 10 7.89543 10 9C10 10.1046 9.10457 11 8 11Z"></path>
+          </svg>
+          <div class="card__content">
+            <p class="card__title">Card Title</p>
+            <p class="card__description">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco.
+            </p>
+          </div>
+        </div>
+
+
+
+
+      </div>
+
+
     </>
   );
 }
